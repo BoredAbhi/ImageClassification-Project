b17dc4ca2fc2d4c359b893948a74cec84a629dd6
Abhijeet Bhattacharjee <teejihba8@gmail.com>
2020-08-18 23:20:50 +0530

    Finishing code restructuring

--- src/training/train.py
+++ src/training/train.py
@@ -7,13 +7,15 @@ Created on Tue Aug 18 10:20:49 2020
 
 import h5py
 import scipy
+import argparse
 import numpy as np
 from PIL import Image
-from scipy import ndimage
+from imageio import imread
 import matplotlib.pyplot as plt
-from src.utils_initialize_parameters import initialize_parameters
+from sklearn.metrics import classification_report, f1_score
+from src.utils_initialize_parameters import initialize_parameters, initialize_parameters_deep
 from src.utils_forward_propagation import linear_activation_forward, L_model_forward
-from src.utils_back_propagation import linear_activation_backward
+from src.utils_back_propagation import linear_activation_backward, L_model_backward
 from src.utils_cost_function_update_parameters import compute_cost, update_parameters
 
 def load_data():
@@ -67,7 +69,7 @@ def two_layer_model(X, Y, layers_dims, learning_rate = 0.0075, num_iterations = 
     b2 = parameters["b2"]
     
     # Loop (gradient descent)
-
+    print('Training in progress ...')
     for i in range(0, num_iterations):
 
         # Forward propagation: LINEAR -> RELU -> LINEAR -> SIGMOID. Inputs: "X, W1, b1, W2, b2". Output: "A1, cache1, A2, cache2".
@@ -110,13 +112,14 @@ def two_layer_model(X, Y, layers_dims, learning_rate = 0.0075, num_iterations = 
         # Print the cost every 100 training example
         if print_cost and i % 100 == 0:
             print("Cost after iteration {}: {}".format(i, np.squeeze(cost)))
-        if print_cost and i % 100 == 0:
+        if i % 100 == 0:
             costs.append(cost)
-       
+    print ("Cost after final iteration", costs[-1]) 
+          
     # plot the cost
 
     squeezed_cost = np.squeeze(costs)
-    print(squeezed_cost)
+    # print(squeezed_cost)
     plt.plot(squeezed_cost)
     plt.ylabel('cost')
     plt.xlabel('iterations (per hundreds)')
@@ -156,12 +159,78 @@ def predict(X, y, parameters):
     #print results
     #print ("predictions: " + str(p))
     #print ("true labels: " + str(y))
-    print("Accuracy: "  + str(np.sum((p == y)/m)))
-        
+    
+    # print("Accuracy: "  + str(np.sum((p == y)/m)))
+    
+    
+    
     return p
 
 
-def run():
+def L_layer_model(X, Y, layers_dims, learning_rate = 0.0075, num_iterations = 3000, print_cost=False):#lr was 0.009
+    """
+    Implements a L-layer neural network: [LINEAR->RELU]*(L-1)->LINEAR->SIGMOID.
+    
+    Arguments:
+    X -- data, numpy array of shape (num_px * num_px * 3, number of examples)
+    Y -- true "label" vector (containing 0 if cat, 1 if non-cat), of shape (1, number of examples)
+    layers_dims -- list containing the input size and each layer size, of length (number of layers + 1).
+    learning_rate -- learning rate of the gradient descent update rule
+    num_iterations -- number of iterations of the optimization loop
+    print_cost -- if True, it prints the cost every 100 steps
+    
+    Returns:
+    parameters -- parameters learnt by the model. They can then be used to predict.
+    """
+
+    np.random.seed(1)
+    costs = []                         # keep track of cost
+    
+    # Parameters initialization. (≈ 1 line of code)
+    ### START CODE HERE ###
+    parameters = initialize_parameters_deep(layers_dims)
+    ### END CODE HERE ###
+    
+    # Loop (gradient descent)
+    for i in range(0, num_iterations):
+
+        # Forward propagation: [LINEAR -> RELU]*(L-1) -> LINEAR -> SIGMOID.
+        ### START CODE HERE ### (≈ 1 line of code)
+        AL, caches = L_model_forward(X, parameters)
+        ### END CODE HERE ###
+        
+        # Compute cost.
+        ### START CODE HERE ### (≈ 1 line of code)
+        cost = compute_cost(AL, Y)
+        ### END CODE HERE ###
+    
+        # Backward propagation.
+        ### START CODE HERE ### (≈ 1 line of code)
+        grads = L_model_backward(AL, Y, caches)
+        ### END CODE HERE ###
+ 
+        # Update parameters.
+        ### START CODE HERE ### (≈ 1 line of code)
+        parameters = update_parameters(parameters, grads, learning_rate)
+        ### END CODE HERE ###
+                
+        # Print the cost every 100 training example
+        if print_cost and i % 100 == 0:
+            print ("Cost after iteration %i: %f" %(i, cost))
+        if print_cost and i % 100 == 0:
+            costs.append(cost)
+            
+            
+    # plot the cost
+    plt.plot(np.squeeze(costs))
+    plt.ylabel('cost')
+    plt.xlabel('iterations (per hundreds)')
+    plt.title("Learning rate =" + str(learning_rate))
+    plt.show()
+    
+    return parameters
+
+def run(args):
     
     train_x_orig, train_y, test_x_orig, test_y, classes = load_data()
     
@@ -171,11 +240,13 @@ def run():
     plt.imshow(train_x_orig[index])
     print ("y = " + str(train_y[0,index]) + ". It's a " + classes[train_y[0,index]].decode("utf-8") +  " picture.")
     
-    
+    '''
     # Explore the dataset 
     m_train = train_x_orig.shape[0]
     num_px = train_x_orig.shape[1]
     m_test = test_x_orig.shape[0]
+    
+    #print('num_px=', num_px)  
     
     # print ("Number of training examples: " + str(m_train))
     # print ("Number of testing examples: " + str(m_test))
@@ -184,7 +255,7 @@ def run():
     # print ("train_y shape: " + str(train_y.shape))
     # print ("test_x_orig shape: " + str(test_x_orig.shape))
     # print ("test_y shape: " + str(test_y.shape))
-    '''
+    
     
     # Reshape the training and test examples 
     train_x_flatten = train_x_orig.reshape(train_x_orig.shape[0], -1).T   # The "-1" makes reshape flatten the remaining dimensions
@@ -194,20 +265,65 @@ def run():
     train_x = train_x_flatten/255.
     test_x = test_x_flatten/255.
     
-    print ("train_x's shape: " + str(train_x.shape))
-    print ("test_x's shape: " + str(test_x.shape))
+    #print ("train_x's shape: " + str(train_x.shape))
+    #print ("test_x's shape: " + str(test_x.shape))
     
     ### CONSTANTS DEFINING THE MODEL ####
     n_x = 12288     # num_px * num_px * 3
     n_h = 7
     n_y = 1
     layers_dims = (n_x, n_h, n_y)
-
-    parameters = two_layer_model(train_x, train_y, layers_dims = (n_x, n_h, n_y), num_iterations = 2500, print_cost=True)
+    
+    #setting printCost to False
+    parameters = two_layer_model(train_x, train_y, layers_dims = (n_x, n_h, n_y), num_iterations = 2500, print_cost=False)
     
     predictions_train = predict(train_x, train_y, parameters)
     
     predictions_test = predict(test_x, test_y, parameters)
+    '''
     
+    #  4-layer model
+    
+    layers_dims = [12288, 20, 7, 5, 1] 
+    
+    parameters = L_layer_model(train_x, train_y, layers_dims, num_iterations = 1500, print_cost = True)
+    
+    '''
+    
+    
+    my_image = args.image # change this to the name of your image file
+    my_label_y = [1] # the true class of your image (1 -> cat, 0 -> non-cat)
+    ## END CODE HERE ## 
+    
+    full_name = "images/" + my_image
+    #fname = my_image
+    image = Image.open(full_name)
+    #print(np.array(image).shape)
+    #image = np.array(ndimage.imread(fname, flatten=False))
+    
+    
+    
+    resized_image = image.resize((num_px,num_px))
+    #print(my_image, type(my_image))
+    #print(np.array(my_image).shape)
+    #my_image.show()
+    
+    resized_image = np.delete(resized_image, np.s_[3:], 2)
+    #print(np.array(my_image).shape)
+    #print(my_image[1,1])
+    
+    flatten_image = np.array(resized_image).reshape((num_px*num_px*3,1))
+    flatten_image = flatten_image/255.
+    my_predicted_image = predict(flatten_image, my_label_y, parameters)
+    
+    plt.imshow(image)
+    print ("y = " + str(np.squeeze(my_predicted_image)) + ", the L-layer model predicted ", my_image, "as a \"" + classes[int(np.squeeze(my_predicted_image)),].decode("utf-8") +  "\" picture.")
+    
+        
 if __name__ == "__main__":
-    run()
+    parser = argparse.ArgumentParser(
+        'Character Based CNN for text classification')
+    parser.add_argument('--image', type=str,
+                        default='my_image.png')
+    args = parser.parse_args()
+    run(args)
